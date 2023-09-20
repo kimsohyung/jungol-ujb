28d7e069aff5088648cc4b7898da6215e951503b
21-manic-013g <21-manic-013g@21-manic-013>
2023-09-20 12:34:15 +0900

    510번 문제풀이

--- Jungol/src/q510/Main.java
+++ Jungol/src/q510/Main.java
@@ -1,7 +1,7 @@
 package q510;
 
 public class Main {
-
+//정수형 변수 2개를 선언하여 -1과 100을 대입한 후 아래와 같이 출력하는 프로그램을 작성하라.
 	public static void main(String[] args) {
 		int a= -1;
 		int b = 100;
@@ -10,6 +10,8 @@ public class Main {
 		//int a,b
 		//a=-1 b=100
 		
+		a=99;
+		System.out.println(a);
 	}
 
 }
