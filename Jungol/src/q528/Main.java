30cdfda12a4ac7e642a47028371bf4e4a158c5a2
21-manic-013g <21-manic-013g@21-manic-013>
2023-09-20 15:33:55 +0900

    퀴즈

---
+++ Jungol/src/q528/Main.java
@@ -0,0 +1,22 @@
+package q528;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String strX = scanner.nextLine();
+		int x = Integer.parseInt(strX);
+		
+		if(x>0) {
+			System.out.println(x);
+		}else {
+			System.out.println(x);
+			System.out.println("minus");
+		}
+
+	}
+
+}
---
+++ Jungol/src/q9006/Main.java
@@ -0,0 +1,25 @@
+package q9006;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 123;
+		double b = 6.500000;
+		
+		// TODO Auto-generated method stub
+		System.out.println("수를 출력할 때는 따옴표를 생략합니다.");
+		System.out.println(a);
+		System.out.printf("%.4f\n",b);
+		/*  
+		 * %d = 정수
+		 * %f = 실수
+		 * %s = 문자열 
+		 * %c = 문자
+		 * \n = 줄바꿈
+		 * 
+		 */
+		
+	}
+
+}
