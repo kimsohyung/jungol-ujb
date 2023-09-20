dde6d328b538ce8012010635b27d4c1e21fff011
21-manic-013g <21-manic-013g@21-manic-013>
2023-09-20 12:29:59 +0900

    510번 문제 풀이

---
+++ Jungol/src/q510/Main.java
@@ -0,0 +1,15 @@
+package q510;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a= -1;
+		int b = 100;
+		System.out.println(a);
+		System.out.print(b);
+		//int a,b
+		//a=-1 b=100
+		
+	}
+
+}
